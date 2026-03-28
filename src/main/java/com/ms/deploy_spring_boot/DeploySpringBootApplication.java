e10dee6f5c50a2d2baabaffec7ca1a7c8f299b3e
sourav-18 <souravhd2@gmail.com>
2026-03-28 18:21:54 +0530

    first

---
+++ src/main/java/com/ms/deploy_spring_boot/DeploySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ms.deploy_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/deploy_spring_boot/DeploySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.deploy_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
