a29635f95f2fc8c4ac7162803b02b230ae2bfb4c
sourav-18 <souravhd2@gmail.com>
2026-03-28 18:26:05 +0530

    first

---
+++ src/main/java/com/ms/deploy_spring_boot/BasicController.java
@@ -0,0 +1,14 @@
+package com.ms.deploy_spring_boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/basic")
+public class BasicController {
+    @GetMapping
+    public String getBasic(){
+        return "basic work";
+    }
+}
