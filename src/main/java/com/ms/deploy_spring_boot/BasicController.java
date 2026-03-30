270801d2747c9b3271bff4ccba48e93a750aecad
sourav-18 <souravhd2@gmail.com>
2026-03-30 19:27:46 +0530

    pid add to chek load balancer

--- src/main/java/com/ms/deploy_spring_boot/BasicController.java
+++ src/main/java/com/ms/deploy_spring_boot/BasicController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/basic")
 public class BasicController {
     @GetMapping
-    public String getBasic(){
+    public String getBasic() throws UnknownHostException {
+        String host = InetAddress.getLocalHost().getHostName();
         long pid=ProcessHandle.current().pid();
-        return "basic work Pid: "+pid;
+        String instance = System.getenv("INSTANCE_NAME");
+        return "instance: "+ instance+" basic work Pid: "+pid+" host: "+host;
     }
 }
